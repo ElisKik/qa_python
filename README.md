23872cc9c974da302d9ec4710399cca4cf96f494
Elis Kik <kik_elis@mail.ru>
2023-03-18 21:55:39 +0300

    Add TestBooksCollector

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# qa_python
+Тесты класса `BooksCollector`
+===
+
+# Как запустить
+
+1. Открыть терминал.
+2. Склонировать репозиторий.
+3. Перейти в директорию репозитория с помощью команды `cd`
+4. Развернуть virtual environment:
+   ```bash
+   python -m venv .venv
+   ```
+5. Запустить virtual environment:
+   ```bash
+   source .venv/bin/activate
+   ```
+6. Установить зависимость (`pytest`) внутри virtual environment:
+   ```bash
+   pip install -r requirements.txt
+   ```
+7. Запустить тесты:
+   ```bash
+   pytest -v tests.py
+   ```
+8. Убедиться в том, что все тесты прошлись.
+    <details>
+        <summary>Вывод должен быть следующим...</summary>
+
+    ```bash
+    ============================= test session starts ==============================
+    ...
+    tests.py::TestBooksCollector::test_add_new_book_multiple_count_equals[names0-1] PASSED [  8%]
+    tests.py::TestBooksCollector::test_add_new_book_multiple_count_equals[names1-2] PASSED [ 16%]
+    tests.py::TestBooksCollector::test_add_new_book_twice_count_equals_one PASSED [ 25%]
+    tests.py::TestBooksCollector::test_set_book_rating_not_added_not_changed PASSED [ 33%]
+    tests.py::TestBooksCollector::test_set_book_rating_out_of_bound_not_changed[-1] PASSED [ 41%]
+    tests.py::TestBooksCollector::test_set_book_rating_out_of_bound_not_changed[0] PASSED [ 50%]
+    tests.py::TestBooksCollector::test_set_book_rating_out_of_bound_not_changed[11] PASSED [ 58%]
+    tests.py::TestBooksCollector::test_get_book_rating_not_added_is_none PASSED [ 66%]
+    tests.py::TestBooksCollector::test_add_book_in_favorites_multiple_count_equals[names0-1] PASSED [ 75%]
+    tests.py::TestBooksCollector::test_add_book_in_favorites_multiple_count_equals[names1-2] PASSED [ 83%]
+    tests.py::TestBooksCollector::test_add_book_in_favorites_not_added_not_found PASSED [ 91%]
+    tests.py::TestBooksCollector::test_delete_book_from_favorites_not_found PASSED [100%]
+
+    ============================== 12 passed in 0.02s ==============================
+    ```
+
+    </details>
+
+# Реализованные тесты
+
+- [x] Проверка добавления книг
+- [x] Нельзя добавить одну и ту же книгу дважды
+- [x] Нельзя выставить рейтинг книге, которой нет в списке
+- [x] Нельзя выставить рейтинг меньше 1
+- [x] Нельзя выставить рейтинг больше 10
+- [x] У не добавленной книги нет рейтинга
+- [x] Добавление книги в избранное
+- [x] Нельзя добавить книгу в избранное, если её нет в словаре `books_rating`
+- [x] Проверка удаления книги из избранного
--- tests.py
+++ tests.py
@@ -1,24 +1,109 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
+    @pytest.fixture(autouse=True)
+    def fixture_books_collector(self):
+        self.books_collector = BooksCollector()
+        return self.books_collector
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    @pytest.mark.parametrize(
+        'names, expected_count',
+        [
+            [ [ 'Щегол' ], 1 ],
+            [ [ 'Послемрак', '1Q84' ], 2 ]
+        ]
+    )
+    def test_add_new_book_multiple_count_equals(self, names, expected_count):
+        for name in names:
+            self.books_collector.add_new_book(name)
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        books_rating = self.books_collector.get_books_rating()
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(books_rating) == expected_count
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_twice_count_equals_one(self):
+        name = 'Щегол'
+
+        self.books_collector.add_new_book(name)
+        self.books_collector.add_new_book(name)
+
+        books_rating = self.books_collector.get_books_rating()
+
+        assert len(books_rating) == 1
+
+    def test_set_book_rating_not_added_not_changed(self):
+        added_name = 'Щегол'
+        self.books_collector.add_new_book(added_name)
+
+        not_added_name = '1Q84'
+        self.books_collector.set_book_rating(not_added_name, 5)
+
+        books_rating = self.books_collector.get_books_rating()
+
+        assert not_added_name not in books_rating.keys()
+
+    @pytest.mark.parametrize(
+        'wrong_rating',
+        [
+            -1,
+            0,
+            11
+        ]
+    )
+    def test_set_book_rating_out_of_bound_not_changed(self, wrong_rating):
+        name = 'Щегол'
+
+        self.books_collector.add_new_book(name)
+        self.books_collector.set_book_rating(name, wrong_rating)
+
+        book_rating = self.books_collector.get_book_rating(name)
+
+        assert book_rating is not wrong_rating
+
+    def test_get_book_rating_not_added_is_none(self):
+        name = 'Щегол'
+
+        book_rating = self.books_collector.get_book_rating(name)
+
+        assert book_rating is None
+
+    @pytest.mark.parametrize(
+        'names, expected_count',
+        [
+            [ [ 'Щегол' ], 1 ],
+            [ [ 'Послемрак', '1Q84' ], 2 ]
+        ]
+    )
+    def test_add_book_in_favorites_multiple_count_equals(self, names, expected_count):
+        for name in names:
+            self.books_collector.add_new_book(name)
+            self.books_collector.add_book_in_favorites(name)
+
+        favorite_books = self.books_collector.get_list_of_favorites_books()
+
+        assert len(favorite_books) == expected_count
+
+    def test_add_book_in_favorites_not_added_not_found(self):
+        name = 'Щегол'
+
+        self.books_collector.add_book_in_favorites(name)
+
+        books_rating = self.books_collector.get_books_rating()
+        favorite_books = self.books_collector.get_list_of_favorites_books()
+
+        assert name not in books_rating and \
+               name not in favorite_books
+
+    def test_delete_book_from_favorites_not_found(self):
+        name = 'Щегол'
+
+        self.books_collector.add_new_book(name)
+        self.books_collector.add_book_in_favorites(name)
+        self.books_collector.delete_book_from_favorites(name)
+
+        favorite_books = self.books_collector.get_list_of_favorites_books()
+
+        assert name not in favorite_books
+
