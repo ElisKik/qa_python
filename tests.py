5519c1c86b4f0f919773911aa5359849a272ef9a
Elis Kik <kik_elis@mail.ru>
2023-03-25 18:38:57 +0300

    Add messages to asserts

--- tests.py
+++ tests.py
@@ -14,7 +14,9 @@ class TestBooksCollector:
 
         books_rating = books_collector.get_books_rating()
 
-        assert len(books_rating) == expected_count
+        books_count = len(books_rating)
+
+        assert books_count == expected_count, f'Expected count of books in rating: {expected_count}, got: {books_count}'
 
     def test_add_new_book_twice_count_equals_one(self, books_collector):
         name = 'Щегол'
@@ -24,7 +26,10 @@ class TestBooksCollector:
 
         books_rating = books_collector.get_books_rating()
 
-        assert len(books_rating) == 1
+        books_count = len(books_rating)
+        expected_count = 1
+
+        assert books_count == expected_count, f'Expected count of books in rating: {expected_count}, got: {books_count}'
 
     def test_set_book_rating_not_added_not_changed(self, books_collector):
         added_name = 'Щегол'
@@ -35,7 +40,7 @@ class TestBooksCollector:
 
         books_rating = books_collector.get_books_rating()
 
-        assert not_added_name not in books_rating.keys()
+        assert not_added_name not in books_rating.keys(), f'Book {not_added_name} must not be in books rating, because it was not added'
 
     @pytest.mark.parametrize(
         'wrong_rating',
@@ -53,14 +58,14 @@ class TestBooksCollector:
 
         book_rating = books_collector.get_book_rating(name)
 
-        assert book_rating is not wrong_rating
+        assert book_rating is not wrong_rating, f'Book {name} added with the wrong rating {wrong_rating}'
 
     def test_get_book_rating_not_added_is_none(self, books_collector):
         name = 'Щегол'
 
         book_rating = books_collector.get_book_rating(name)
 
-        assert book_rating is None
+        assert book_rating is None, f'Book {name} must not be in books rating, because it was not added'
 
     @pytest.mark.parametrize(
         'names, expected_count',
@@ -76,7 +81,9 @@ class TestBooksCollector:
 
         favorite_books = books_collector.get_list_of_favorites_books()
 
-        assert len(favorite_books) == expected_count
+        books_count = len(favorite_books)
+
+        assert books_count == expected_count, f'Expected count of favorite books: {expected_count}, got: {books_count}'
 
     def test_add_book_in_favorites_not_added_not_found(self, books_collector):
         name = 'Щегол'
@@ -86,8 +93,8 @@ class TestBooksCollector:
         books_rating = books_collector.get_books_rating()
         favorite_books = books_collector.get_list_of_favorites_books()
 
-        assert name not in books_rating and \
-               name not in favorite_books
+        assert name not in books_rating, f'Book {name} must not be in books rating, because it was not added'
+        assert name not in favorite_books, f'Book {name} must not be in favorites, because it was not added'
 
     def test_delete_book_from_favorites_not_found(self, books_collector):
         name = 'Щегол'
@@ -98,5 +105,4 @@ class TestBooksCollector:
 
         favorite_books = books_collector.get_list_of_favorites_books()
 
-        assert name not in favorite_books
-
+        assert name not in favorite_books, f'Book {name} must not be in favorites, because it was deleted'
